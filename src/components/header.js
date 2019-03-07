fcd3cdb81d41cbcfbbb25c936e2f14b7b62b71da
Adam Goth <adamgoth@gmail.com>
2019-03-06 20:35:42 -0700

    Initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,15 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import Helmet from "react-helmet"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+const Header = () => (
+  <header>
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css?family=Muli"
+        rel="stylesheet"
+      />
+    </Helmet>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
---
+++ src/components/landing.js
@@ -0,0 +1,110 @@
+import React from "react"
+import Img from "gatsby-image"
+import { StaticQuery, graphql } from "gatsby"
+
+import "./landing.scss"
+
+export default () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          me: file(relativePath: { eq: "me.png" }) {
+            childImageSharp {
+              fixed(width: 200) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+          github: file(relativePath: { eq: "social-github.png" }) {
+            childImageSharp {
+              fixed(width: 28) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+          linkedin: file(relativePath: { eq: "social-linkedin.png" }) {
+            childImageSharp {
+              fixed(width: 28) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+          twitter: file(relativePath: { eq: "social-twitter.png" }) {
+            childImageSharp {
+              fixed(width: 28) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+          instagram: file(relativePath: { eq: "social-instagram.png" }) {
+            childImageSharp {
+              fixed(width: 28) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+          email: file(relativePath: { eq: "social-email.png" }) {
+            childImageSharp {
+              fixed(width: 28) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+        }
+      `}
+      render={data => (
+        <div className="Landing">
+          <Img
+            className="Landing-image"
+            fixed={data.me.childImageSharp.fixed}
+          />
+          <h1 className="Landing-name">@adamgoth</h1>
+          <p className="Landing-desc">a Denver-based developer</p>
+          <div className="Landing-social">
+            <a
+              className="Landing-icon"
+              href="#"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <Img fixed={data.github.childImageSharp.fixed} />
+            </a>
+            <a
+              className="Landing-icon"
+              href="#"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <Img fixed={data.linkedin.childImageSharp.fixed} />
+            </a>
+            <a
+              className="Landing-icon"
+              href="#"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <Img fixed={data.twitter.childImageSharp.fixed} />
+            </a>
+            <a
+              className="Landing-icon"
+              href="#"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <Img fixed={data.instagram.childImageSharp.fixed} />
+            </a>
+            <a
+              className="Landing-icon"
+              href="#"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <Img fixed={data.email.childImageSharp.fixed} />
+            </a>
+          </div>
+        </div>
+      )}
+    />
+  )
+}
