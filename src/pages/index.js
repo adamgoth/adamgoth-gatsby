c72520b6107bf5b3f9bdb21e6e5e312e0089bb8f
Adam Goth <adamgoth@gmail.com>
2019-08-13 18:12:33 -0600

    Update packages, update title

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO
-      title="home"
+      title="adamgoth.com"
       keywords={[`developer`, `portfolio`, `denver`, `web development`]}
     />
     <Landing />
