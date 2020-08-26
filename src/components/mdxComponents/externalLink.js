419c7b40a521462a72ffcf0a683ef32b732cf89b
adamgoth <adamgoth@gmail.com>
2020-08-25 20:37:12 -0600

    Add tweet URL link

--- src/components/mdxComponents/externalLink.js
+++ src/components/mdxComponents/externalLink.js
@@ -1,9 +1,11 @@
 import React from "react"
 
 export default props => {
-  if (props.href.includes("adamgoth.com") || props.href[0] === "/") {
-    return <a href={props.href}>{props.children}</a>
-  }
+  // for use with internal links
+  // comment out for now
+  // if (props.href.includes("adamgoth.com") || props.href[0] === "/") {
+  //   return <a href={props.href}>{props.children}</a>
+  // }
   return (
     <a href={props.href} target="_blank" rel="noopener noreferrer">
       {props.children}
