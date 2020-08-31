3becb3510ebf937674c7ec055f631ebd2b559e6d
adamgoth <adamgoth@gmail.com>
2020-08-30 19:54:39 -0600

    Add twitter metadata

--- src/components/posts-page-layout.js
+++ src/components/posts-page-layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import Helmet from "react-helmet"
 import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import Layout from "../components/layout"
@@ -9,18 +10,25 @@ import "./posts-page-layout.scss"
 
 export default function PageTemplate({ data: { mdx } }) {
   return (
-    <Layout>
-      <div style={{ padding: "0 1rem" }}>
-        <h1>{mdx.frontmatter.title}</h1>
-        <h4 className="template__post-info">{`${mdx.frontmatter.date} by ${mdx.frontmatter.author}`}</h4>
-        <MDXProvider components={components}>
-          <MDXRenderer>{mdx.body}</MDXRenderer>
-        </MDXProvider>
-      </div>
-      <Link className="template__link" to={`/blog`} style={{}}>
-        ← Back to posts
-      </Link>
-    </Layout>
+    <>
+      <Helmet>
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={mdx.frontmatter.title} />
+        <meta name="twitter:description" content={mdx.frontmatter.preview} />
+      </Helmet>
+      <Layout>
+        <div style={{ padding: "0 1rem" }}>
+          <h1>{mdx.frontmatter.title}</h1>
+          <h4 className="template__post-info">{`${mdx.frontmatter.date} by ${mdx.frontmatter.author}`}</h4>
+          <MDXProvider components={components}>
+            <MDXRenderer>{mdx.body}</MDXRenderer>
+          </MDXProvider>
+        </div>
+        <Link className="template__link" to={`/blog`} style={{}}>
+          ← Back to posts
+        </Link>
+      </Layout>
+    </>
   )
 }
 
