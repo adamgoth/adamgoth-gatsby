a9d6eb737b6cace59448bf58cd0027263b26e9c2
adamgoth <adamgoth@gmail.com>
2020-09-21 22:04:22 -0600

    First draft of post

--- src/components/mdxComponents/inlineCode.js
+++ src/components/mdxComponents/inlineCode.js
@@ -1,5 +1,7 @@
 import React from "react"
 
 export default ({ children }) => (
-  <code style={{ background: "#00000018" }}>{children}</code>
+  <code style={{ background: "#00000018", padding: "0 0.2rem" }}>
+    {children}
+  </code>
 )
