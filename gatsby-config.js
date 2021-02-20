04cda4f3cedbec68eeb48ae4611ed6da419803b1
adamgoth <adamgoth@gmail.com>
2021-02-20 11:44:08 -0700

    Add GA vitals plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,10 @@
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `adamgoth.com`,
@@ -24,5 +31,21 @@ module.exports = {
       },
     },
     `gatsby-plugin-mdx`,
+    {
+      resolve: "gatsby-plugin-web-vitals",
+      options: {
+        // The Google Analytics property ID; the reporting code won't be generated without it
+        trackingId: process.env.GA_TRACKING_ID,
+        // An array with metrics you want to track and send to analytics
+        metrics: [`FID`, `TTFB`, `LCP`, `CLS`, `FCP`],
+        // Event Category (optional) { string }, default 'Web Vitals'
+        eventCategory: "Performance",
+        // Include Web Vitals tracking in development
+        // Defaults to false meaning Vitals will only be tracked in production.
+        includeInDevelopment: false,
+        // Prints metrics in the console when true
+        debug: false,
+      },
+    },
   ],
 }
